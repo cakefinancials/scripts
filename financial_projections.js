e78560dafe7a684992ded290d7c2651aa95050ec
Samuel Perez <samuelp@socialnative.com>
2018-11-01 14:36:34 -0700

    formatting

--- financial_projections.js
+++ financial_projections.js
@@ -1,120 +1,112 @@
 const fs = require('fs');
-const plotly = require('plotly')('samuel.e.perez', /* NO NO */);
+const plotly = require('plotly')('samuel.e.perez' /* NO NO */);
 const program = require('commander');
 const util = require('util');
 
 program
-    .version('0.1.0')
-    .option('--mu <n>', 'mean of distribution', parseFloat)
-    .option('--sigma <n>', 'sigma of distribution', parseFloat)
-    .option('--upper <n>', 'upper bound in normal return', parseFloat)
-    .option('--lower <n>', 'lower bound in normal return', parseFloat)
-    .option('--discount <n>', 'discount percentage', parseFloat)
-    .option('--period <n>', 'period in months', parseInt)
-    .option('--iterations <n>', 'number of results to simulate', parseInt)
-    .option('--histogram-file <filename>', 'name of histogram file')
-    .parse(process.argv);
+  .version('0.1.0')
+  .option('--mu <n>', 'mean of distribution', parseFloat)
+  .option('--sigma <n>', 'sigma of distribution', parseFloat)
+  .option('--upper <n>', 'upper bound in normal return', parseFloat)
+  .option('--lower <n>', 'lower bound in normal return', parseFloat)
+  .option('--discount <n>', 'discount percentage', parseFloat)
+  .option('--period <n>', 'period in months', parseInt)
+  .option('--iterations <n>', 'number of results to simulate', parseInt)
+  .option('--histogram-file <filename>', 'name of histogram file')
+  .parse(process.argv);
 
 const { mu, sigma, upper, lower, period = 12, discount, iterations, histogramFile } = program;
 
 const normDist = ({ mu = 0, sigma = 1 }) => {
-    var x1, x2, rad;
-    do {
-        x1 = 2 * Math.random() - 1;
-        x2 = 2 * Math.random() - 1;
-        rad = x1 * x1 + x2 * x2;
-    } while (rad >= 1 || rad == 0);
-    var c = Math.sqrt(-2 * Math.log(rad) / rad);
-    return sigma * x1 * c + mu;
+  var x1, x2, rad;
+  do {
+    x1 = 2 * Math.random() - 1;
+    x2 = 2 * Math.random() - 1;
+    rad = x1 * x1 + x2 * x2;
+  } while (rad >= 1 || rad == 0);
+  var c = Math.sqrt((-2 * Math.log(rad)) / rad);
+  return sigma * x1 * c + mu;
 };
 
 const ratio = 12 / period;
 
 const getSampleReturn = ({ mu, sigma, upper, lower }) => {
-    let sample;
-    do {
-        sample = normDist({ mu: mu / ratio, sigma: sigma / Math.sqrt(ratio) });
-    } while (!(lower < sample && sample < upper));
+  let sample;
+  do {
+    sample = normDist({ mu: mu / ratio, sigma: sigma / Math.sqrt(ratio) });
+  } while (!(lower < sample && sample < upper));
 
-    const periodReturn = (1 + sample) / (1 - discount);
-    return {
-        sample,
-        periodReturn,
-        annualReturn: Math.pow(periodReturn, ratio)
-    };
+  const periodReturn = (1 + sample) / (1 - discount);
+  return {
+    sample,
+    periodReturn,
+    annualReturn: Math.pow(periodReturn, ratio),
+  };
 };
 
 const getHistogram = async ({ samples }) => {
-    const trace1 = {
-        x: samples,
-        type: 'histogram'
-    };
+  const trace1 = {
+    x: samples,
+    type: 'histogram',
+  };
 
-    var figure = { 'data': [ trace1 ] };
+  var figure = { data: [trace1] };
 
-    var imgOpts = {
-        format: 'png',
-        width: 1000,
-        height: 500
-    };
+  var imgOpts = {
+    format: 'png',
+    width: 1000,
+    height: 500,
+  };
 
-    const imageStream = await util.promisify(plotly.getImage).bind(plotly)(figure, imgOpts);
-    await new Promise(resolve => {
-        imageStream.pipe(fs.createWriteStream(histogramFile))
-            .on('finish', resolve);
-    });
+  const imageStream = await util.promisify(plotly.getImage).bind(plotly)(figure, imgOpts);
+  await new Promise(resolve => {
+    imageStream.pipe(fs.createWriteStream(histogramFile)).on('finish', resolve);
+  });
 };
 
 const getMeanAndVar = ({ samples }) => {
-    const getVariance = (samples, mean) => {
-        const sumOfSquares = samples.reduce(
-            (pre, sample) => {
-                pre = pre + Math.pow((sample - mean), 2);
-                return pre;
-            },
-            0
-        );
+  const getVariance = (samples, mean) => {
+    const sumOfSquares = samples.reduce((pre, sample) => {
+      pre = pre + Math.pow(sample - mean, 2);
+      return pre;
+    }, 0);
 
-        return sumOfSquares / samples.length;
-    };
+    return sumOfSquares / samples.length;
+  };
 
-    const getMean = (samples) => {
-        const meanTot = samples.reduce(
-            (pre, sample) => {
-                return pre + sample;
-            },
-            0
-        );
+  const getMean = samples => {
+    const meanTot = samples.reduce((pre, sample) => {
+      return pre + sample;
+    }, 0);
 
-        return meanTot / samples.length;
-    };
+    return meanTot / samples.length;
+  };
 
-    const mean = getMean(samples);
+  const mean = getMean(samples);
 
-    return {
-        mean,
-        variance: getVariance(samples, mean)
-    };
+  return {
+    mean,
+    variance: getVariance(samples, mean),
+  };
 };
 
-
 (async () => {
-    const samples = [];
-    const periodReturns = [];
-    const annualReturns = [];
-    for (let i = 0; i < iterations; i++) {
-        let { sample, periodReturn, annualReturn } = getSampleReturn({ mu, sigma, upper, lower });
-        samples.push(sample);
-        periodReturns.push(periodReturn);
-        annualReturns.push(annualReturn);
-    }
+  const samples = [];
+  const periodReturns = [];
+  const annualReturns = [];
+  for (let i = 0; i < iterations; i++) {
+    let { sample, periodReturn, annualReturn } = getSampleReturn({ mu, sigma, upper, lower });
+    samples.push(sample);
+    periodReturns.push(periodReturn);
+    annualReturns.push(annualReturn);
+  }
 
-    console.log('SAMPLES STATS:');
-    console.log(getMeanAndVar({ samples: samples }));
-    console.log('PERIOD RETURNS STATS:');
-    console.log(getMeanAndVar({ samples: periodReturns }));
-    console.log('ANNUAL RETURNS STATS:');
-    console.log(getMeanAndVar({ samples: annualReturns }));
+  console.log('SAMPLES STATS:');
+  console.log(getMeanAndVar({ samples: samples }));
+  console.log('PERIOD RETURNS STATS:');
+  console.log(getMeanAndVar({ samples: periodReturns }));
+  console.log('ANNUAL RETURNS STATS:');
+  console.log(getMeanAndVar({ samples: annualReturns }));
 
-    //await getHistogram({ samples: returns });
+  //await getHistogram({ samples: returns });
 })().then(() => console.log('done'));
--- user_info.js
+++ user_info.js
@@ -17,248 +17,241 @@ const moment = require('moment');
 const ENVIRONMENT = process.env['NODE_ENV'] || 'dev';
 
 const getS3Contents = ({ s3Key, async = false }) => {
-    try {
-        const command = `aws s3 cp ${s3Key} -`;
-        if (async) {
-            return execAsync(command, { stdio: 'pipe' }).then(result => {
-                return result.stdout;
-            });
-        } else {
-            return new String(execSync(command, { stdio : 'pipe' }));
-        }
-    } catch (e) {
-        return '';
+  try {
+    const command = `aws s3 cp ${s3Key} -`;
+    if (async) {
+      return execAsync(command, { stdio: 'pipe' }).then(result => {
+        return result.stdout;
+      });
+    } else {
+      return new String(execSync(command, { stdio: 'pipe' }));
     }
+  } catch (e) {
+    return '';
+  }
 };
 
-const printObject = (obj) => {
-    const keys = Object.keys(obj);
-    const lines = [];
-    keys.forEach((key) => lines.push(`${key}: ${obj[key]}`));
-    return lines;
+const printObject = obj => {
+  const keys = Object.keys(obj);
+  const lines = [];
+  keys.forEach(key => lines.push(`${key}: ${obj[key]}`));
+  return lines;
 };
 
 let stripeSecret, plaidClientId, plaidSecret, plaidPublicKey, plaidEnv, privateKey;
 
 const main = async () => {
-    console.log('Loading...');
-    ([
-        stripeSecret,
-        plaidClientId,
-        plaidSecret,
-        plaidPublicKey,
-        plaidEnv,
-        privateKey
-    ] = await Promise.all([
-        getS3Contents({ s3Key: `s3://cake-financials-config/${ENVIRONMENT}/STRIPE_PRIVATE_KEY`, async: true }),
-        getS3Contents({ s3Key: 's3://cake-financials-config/PLAID_CLIENT_ID', async: true }),
-        getS3Contents({ s3Key: `s3://cake-financials-config/${ENVIRONMENT}/PLAID_SECRET_KEY`, async: true }),
-        getS3Contents({ s3Key: 's3://cake-financials-config/PLAID_PUBLIC_KEY', async: true }),
-        getS3Contents({ s3Key: `s3://cake-financials-config/${ENVIRONMENT}/PLAID_ENVIRONMENT`, async: true }),
-        getS3Contents({ s3Key: 's3://cake-financials-config/USER_DATA_PRIVATE_KEY', async: true })
-    ]));
+  console.log('Loading...');
+  [stripeSecret, plaidClientId, plaidSecret, plaidPublicKey, plaidEnv, privateKey] = await Promise.all([
+    getS3Contents({ s3Key: `s3://cake-financials-config/${ENVIRONMENT}/STRIPE_PRIVATE_KEY`, async: true }),
+    getS3Contents({ s3Key: 's3://cake-financials-config/PLAID_CLIENT_ID', async: true }),
+    getS3Contents({ s3Key: `s3://cake-financials-config/${ENVIRONMENT}/PLAID_SECRET_KEY`, async: true }),
+    getS3Contents({ s3Key: 's3://cake-financials-config/PLAID_PUBLIC_KEY', async: true }),
+    getS3Contents({ s3Key: `s3://cake-financials-config/${ENVIRONMENT}/PLAID_ENVIRONMENT`, async: true }),
+    getS3Contents({ s3Key: 's3://cake-financials-config/USER_DATA_PRIVATE_KEY', async: true }),
+  ]);
 
-    console.log('Ready to go...');
+  console.log('Ready to go...');
 
-    const privateKeyPassword = readlineSync.question(
-        'What is the password for the private key? ',
-        { hideEchoBack: true }
-    );
+  const privateKeyPassword = readlineSync.question('What is the password for the private key? ', {
+    hideEchoBack: true,
+  });
 
-    const privKeyObj = openpgp.key.readArmored(privateKey).keys[0];
-    await privKeyObj.decrypt(privateKeyPassword);
+  const privKeyObj = openpgp.key.readArmored(privateKey).keys[0];
+  await privKeyObj.decrypt(privateKeyPassword);
 
-    const processNext = async () => {
-        const email = readlineSync.question(
-            'What is the email of the user you want to process? '
-        );
+  const processNext = async () => {
+    const email = readlineSync.question('What is the email of the user you want to process? ');
 
-        let idLinkKey;
-        try {
-            idLinkKey = new String(execSync(
-                `aws s3 ls s3://cake-financials-user-data/email_to_cognito_id/${email}/`
-            ));
-        } catch (e) {
-            console.log('could not find a user with that email');
-            return;
-        }
-
-        const cognitoIdenity = idLinkKey.split(' ').pop().trim();
-
-        let runWithSameUser = true;
-        while (runWithSameUser) {
-            ({ runWithSameUser } = await runUserCommand({ cognitoIdenity, privKeyObj }));
-        }
-    };
-
-    while (true) {
-        await processNext();
+    let idLinkKey;
+    try {
+      idLinkKey = new String(execSync(`aws s3 ls s3://cake-financials-user-data/email_to_cognito_id/${email}/`));
+    } catch (e) {
+      console.log('could not find a user with that email');
+      return;
     }
+
+    const cognitoIdenity = idLinkKey
+      .split(' ')
+      .pop()
+      .trim();
+
+    let runWithSameUser = true;
+    while (runWithSameUser) {
+      ({ runWithSameUser } = await runUserCommand({ cognitoIdenity, privKeyObj }));
+    }
+  };
+
+  while (true) {
+    await processNext();
+  }
 };
 
 const runUserCommand = async ({ cognitoIdenity, privKeyObj }) => {
-    console.log(R.pipe(
-        R.mapObjIndexed(({ name }, index) => `${index}) ${name}`),
-        R.values,
-        R.join('\n')
-    )(USER_COMMANDS));
-    const commandNumber = readlineSync.question(
-        'What is the command you want to run (type the number, or "exit" to process another user)? '
-    );
+  console.log(
+    R.pipe(
+      R.mapObjIndexed(({ name }, index) => `${index}) ${name}`),
+      R.values,
+      R.join('\n')
+    )(USER_COMMANDS)
+  );
+  const commandNumber = readlineSync.question(
+    'What is the command you want to run (type the number, or "exit" to process another user)? '
+  );
 
-    if (commandNumber === 'exit') {
-        return { runWithSameUser: false };
-    }
-    const commandFn = R.path([ commandNumber, 'fn' ], USER_COMMANDS);
+  if (commandNumber === 'exit') {
+    return { runWithSameUser: false };
+  }
+  const commandFn = R.path([commandNumber, 'fn'], USER_COMMANDS);
 
-    if (!commandFn) {
-        console.log(`'${commandNumber}' was not recognized...`);
-        return { runWithSameUser: true };
-    }
-
-    const commandResult = await commandFn({ cognitoIdenity, privKeyObj });
-    const lines = [ ...commandResult, '\n' ];
-
-    lines.push('hit enter to erase and proceed');
-    const log = logUpdate.create(process.stdout, {
-        showCursor: true
-    });
-
-    log(lines.join('\n'));
-    readlineSync.question();
-    log.clear();
+  if (!commandFn) {
+    console.log(`'${commandNumber}' was not recognized...`);
     return { runWithSameUser: true };
+  }
+
+  const commandResult = await commandFn({ cognitoIdenity, privKeyObj });
+  const lines = [...commandResult, '\n'];
+
+  lines.push('hit enter to erase and proceed');
+  const log = logUpdate.create(process.stdout, {
+    showCursor: true,
+  });
+
+  log(lines.join('\n'));
+  readlineSync.question();
+  log.clear();
+  return { runWithSameUser: true };
 };
 
 const USER_COMMANDS = [
-    {
-        name: 'VIEW PLAID TRANSACTIONS',
-        fn: async function viewPlaidTransaction({ cognitoIdenity }) {
-            const contents = getS3Contents({ s3Key: `s3://cake-financials-user-data/${cognitoIdenity}/user_plaid_data.json` });
-            if (!contents) {
-                return [ 'This user has not linked their bank account' ];
-            }
+  {
+    name: 'VIEW PLAID TRANSACTIONS',
+    fn: async function viewPlaidTransaction({ cognitoIdenity }) {
+      const contents = getS3Contents({
+        s3Key: `s3://cake-financials-user-data/${cognitoIdenity}/user_plaid_data.json`,
+      });
+      if (!contents) {
+        return ['This user has not linked their bank account'];
+      }
 
-            const now = moment();
-            const aMonthAgo = now.subtract(30, 'd');
+      const now = moment();
+      const aMonthAgo = now.subtract(30, 'd');
 
-            const { plaidAccessToken } = JSON.parse(contents);
-            //const plaidAccessToken = 'access-development-c4285019-1257-4825-956e-150b686d0082';
+      const { plaidAccessToken } = JSON.parse(contents);
+      //const plaidAccessToken = 'access-development-c4285019-1257-4825-956e-150b686d0082';
 
-            const lines = [ ];
+      const lines = [];
 
-            const plaidClient = getPlaidClient();
-            try {
-                const transactionsResponse = await plaidClient.getTransactions(
-                    plaidAccessToken,
-                    aMonthAgo.format('YYYY-MM-DD'),
-                    now.format('YYYY-MM-DD'),
-                );
+      const plaidClient = getPlaidClient();
+      try {
+        const transactionsResponse = await plaidClient.getTransactions(
+          plaidAccessToken,
+          aMonthAgo.format('YYYY-MM-DD'),
+          now.format('YYYY-MM-DD')
+        );
 
-                const { transactions } = transactionsResponse;
-                if (transactions.length === 0) {
-                    lines.push('Did not find any transactions');
-                } else {
-                    lines.push('Transactions over last month: (Date: Amount)');
-                    R.forEach(
-                        ({ amount, date }) => lines.push(`${date}: ${amount}`),
-                        transactions
-                    );
-                }
-            } catch (err) {
-                lines.push('An error occurred while fetching transactions from Plaid');
-                lines.push(`${err.error_code}: ${err.error_message}`);
-            }
-
-            const authResponse = await plaidClient.getAuth(plaidAccessToken);
-            const balances = authResponse.accounts[0].balances;
-            lines.push(`Current: ${balances.current}, Available: ${balances.available}`);
-
-            return lines;
+        const { transactions } = transactionsResponse;
+        if (transactions.length === 0) {
+          lines.push('Did not find any transactions');
+        } else {
+          lines.push('Transactions over last month: (Date: Amount)');
+          R.forEach(({ amount, date }) => lines.push(`${date}: ${amount}`), transactions);
         }
+      } catch (err) {
+        lines.push('An error occurred while fetching transactions from Plaid');
+        lines.push(`${err.error_code}: ${err.error_message}`);
+      }
+
+      const authResponse = await plaidClient.getAuth(plaidAccessToken);
+      const balances = authResponse.accounts[0].balances;
+      lines.push(`Current: ${balances.current}, Available: ${balances.available}`);
+
+      return lines;
     },
-    {
-        name: 'VIEW BROKERAGE CREDENTIALS',
-        fn: async function viewBrokerageCredentials({ cognitoIdenity, privKeyObj }) {
-            const brokerageCredentials = getS3Contents({ s3Key: `s3://cake-financials-user-data/${cognitoIdenity}/brokerage_credentials` });
-            const lines = [ ];
+  },
+  {
+    name: 'VIEW BROKERAGE CREDENTIALS',
+    fn: async function viewBrokerageCredentials({ cognitoIdenity, privKeyObj }) {
+      const brokerageCredentials = getS3Contents({
+        s3Key: `s3://cake-financials-user-data/${cognitoIdenity}/brokerage_credentials`,
+      });
+      const lines = [];
 
-            if (brokerageCredentials.length > 0) {
-                const brokerageCredentialsOptions = {
-                    message: openpgp.message.readArmored(brokerageCredentials),
-                    privateKeys: [ privKeyObj ]
-                };
-                const brokerageCredentialsPlaintext = await openpgp.decrypt(brokerageCredentialsOptions);
-                lines.push('Brokerage Credentials:');
+      if (brokerageCredentials.length > 0) {
+        const brokerageCredentialsOptions = {
+          message: openpgp.message.readArmored(brokerageCredentials),
+          privateKeys: [privKeyObj],
+        };
+        const brokerageCredentialsPlaintext = await openpgp.decrypt(brokerageCredentialsOptions);
+        lines.push('Brokerage Credentials:');
 
-                (printObject(JSON.parse(brokerageCredentialsPlaintext.data))).forEach((line) => lines.push(line));
-            } else {
-                lines.push('No brokerage credentials to display');
-            }
+        printObject(JSON.parse(brokerageCredentialsPlaintext.data)).forEach(line => lines.push(line));
+      } else {
+        lines.push('No brokerage credentials to display');
+      }
 
-            return lines;
-        }
+      return lines;
     },
-    {
-        name: 'CHARGE WITH STRIPE',
-        fn: async function viewBrokerageCredentials({ cognitoIdenity }) {
-            const contents = getS3Contents({ s3Key: `s3://cake-financials-user-data/${cognitoIdenity}/user_plaid_data.json` });
-            if (!contents) {
-                return [ 'This user has not linked their bank account' ];
-            }
+  },
+  {
+    name: 'CHARGE WITH STRIPE',
+    fn: async function viewBrokerageCredentials({ cognitoIdenity }) {
+      const contents = getS3Contents({
+        s3Key: `s3://cake-financials-user-data/${cognitoIdenity}/user_plaid_data.json`,
+      });
+      if (!contents) {
+        return ['This user has not linked their bank account'];
+      }
 
-            const amountToCharge = readlineSync.question(
-                'How much do you want to charge? '
-            );
+      const amountToCharge = readlineSync.question('How much do you want to charge? ');
 
-            const parsedAmountToCharge = parseFloat(parseFloat(amountToCharge).toFixed(2));
+      const parsedAmountToCharge = parseFloat(parseFloat(amountToCharge).toFixed(2));
 
-            const answer = readlineSync.question(`Are you sure you want to charge ${parsedAmountToCharge}? Type 'y' for yes: `);
+      const answer = readlineSync.question(
+        `Are you sure you want to charge ${parsedAmountToCharge}? Type 'y' for yes: `
+      );
 
-            const lines = [ ];
-            if (answer !== 'y') {
-                lines.push('Aborting...');
-            } else {
-                // charge with stripe
-                lines.push('Charging...');
-                try {
-                    const stripe = require('stripe')(stripeSecret);
-                    const { plaidAccessToken, plaidAccountId } = JSON.parse(contents);
-                    const plaidClient = getPlaidClient();
+      const lines = [];
+      if (answer !== 'y') {
+        lines.push('Aborting...');
+      } else {
+        // charge with stripe
+        lines.push('Charging...');
+        try {
+          const stripe = require('stripe')(stripeSecret);
+          const { plaidAccessToken, plaidAccountId } = JSON.parse(contents);
+          const plaidClient = getPlaidClient();
 
-                    const response = await plaidClient.createStripeToken(plaidAccessToken, plaidAccountId);
-                    const bankAccountToken = response.stripe_bank_account_token;
+          const response = await plaidClient.createStripeToken(plaidAccessToken, plaidAccountId);
+          const bankAccountToken = response.stripe_bank_account_token;
 
-                    const chargeInCents = Math.floor(parsedAmountToCharge * 100);
+          const chargeInCents = Math.floor(parsedAmountToCharge * 100);
 
-                    const charge = await stripe.charges.create({
-                        amount: chargeInCents,
-                        currency: 'usd',
-                        source: bankAccountToken, // obtained with Stripe.js
-                        description: `Charge for Cake services ending ${moment().format('MMMM Do YYYY')}`
-                    });
+          const charge = await stripe.charges.create({
+            amount: chargeInCents,
+            currency: 'usd',
+            source: bankAccountToken, // obtained with Stripe.js
+            description: `Charge for Cake services ending ${moment().format('MMMM Do YYYY')}`,
+          });
 
-                    lines.push(`Succesfully charged. Charge id: ${charge.id}`);
-                } catch (err) {
-                    console.log('Error while charging, dumping out the error: ', err);
-                }
-            }
-            return lines;
+          lines.push(`Succesfully charged. Charge id: ${charge.id}`);
+        } catch (err) {
+          console.log('Error while charging, dumping out the error: ', err);
         }
-    }
+      }
+      return lines;
+    },
+  },
 ];
 
 const getPlaidClient = () => {
-    const plaid = require('plaid');
+  const plaid = require('plaid');
 
-    var plaidClient = new plaid.Client(
-        plaidClientId,
-        plaidSecret,
-        plaidPublicKey,
-        plaid.environments[plaidEnv]
-    );
+  var plaidClient = new plaid.Client(plaidClientId, plaidSecret, plaidPublicKey, plaid.environments[plaidEnv]);
 
-    return plaidClient;
+  return plaidClient;
 };
 
-main().then(() => console.log('done')).catch(console.error);
+main()
+  .then(() => console.log('done'))
+  .catch(console.error);
